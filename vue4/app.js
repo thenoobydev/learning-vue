0b5aae3f39feb0766c7c3947fde2daa61ef5c016
goelaakash79 <goelaakash79@gmail.com>
2018-10-08 01:13:13 +0530

    commit

---
+++ vue4/app.js
@@ -0,0 +1,28 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    age: 20,
+    a: 0,
+    b: 0
+  },
+  methods:{
+    /*addtoA: function(){
+      console.log("addtoA");
+      return this.a + this.age
+    },
+    addtoB: function(){
+      console.log("addtoB");
+      return this.b + this.age
+    }*/
+  },
+  computed:{
+    addtoA: function(){
+      console.log("addtoA");
+      return this.a + this.age
+    },
+    addtoB: function(){
+      console.log("addtoB");
+      return this.b + this.age
+    }
+  }
+});
